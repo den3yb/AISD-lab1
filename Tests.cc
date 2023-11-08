d83b0aafea31d6263856bba7f1f5d0e86b4b7823
den3yb <126419855+den3yb@users.noreply.github.com>
2023-11-08 08:48:02 +0400

    int test
    int test

--- Tests.cc
+++ Tests.cc
@@ -37,3 +37,30 @@ TEST(Func_Tests, SumStubTest1) {
 	(d.bot_tria()).type();
 	EXPECT_TRUE(true);
 }
+
+
+TEST(Func_Tests, SumStubTest2) {
+	int t1[6]{ 1, 2, 3, 4, 5, 6 };
+	int t2[6]{ 7, 8, 9, 10, 11, 12 };
+	int* p1 = &t1[0];
+	int* p2 = &t2[0];
+	int min = 1;
+	int max = 10;
+	matrix <int> a(2, 3, p1);
+	matrix <int> b(2, 3, min, max);
+	matrix <int> c(3, 2, p2);
+	matrix <int> d(5, 5, min, max);
+	a.type();
+	c.type();
+	std::cout << a(1, 1) << "\n";
+	(a * 2).type();
+	(a + b).type();
+	(b - a).type();
+	(b / 2).type();
+	(a * b).type();
+	(a * c).type();
+	d.type();
+	cout << d.trace() << "\n";
+	(d.bot_tria()).type();
+	EXPECT_TRUE(true);
+}
